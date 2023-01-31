f244fd586a4a6a8822abb03d79a8c8f8237fd396
Leandro <leandrobcrv@gmail.com>
2023-01-31 05:44:15 -0300

    Requisito 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import APIMock from './mock/APImock';
+import { act } from 'react-dom/test-utils';
+import APIProvider from '../context/APIprovider';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  beforeEach(async() => { 
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(APIMock)
+    }); 
+
+    await act(() => {
+      render(
+        <APIProvider>
+        <App />
+        </APIProvider>
+      )
+    })
+  }); 
+
+  it('Testa a caixa para pesquisas na página', async () => {
+    await act(() => render(<App />));
+    const data = await screen.getByTestId('name-filter');
+    expect(data).toBeInTheDocument();
+  })
 });
